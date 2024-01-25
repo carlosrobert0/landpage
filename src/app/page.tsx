2b9ef544ab961dfd7569262c7078cf1ec47691a2
carlosrobert0 <carlosroberto15755@gmail.com>
2024-01-25 13:00:59 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,134 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between bg-[#2A2A2A]">
+        <header className="flex items-center justify-center mt-2.5">
+          <Image
+            alt="Icone"
+            src="/icon.png"
+            width={100}
+            height={87}
+            className="object-contain"
+          />
+        </header>
+        <div className="flex items-center justify-center mt-[60px]">
+          <div>
+            <h4 className="text-[#7ED957] text-xl font-poppins font-semibold">GUIA COMPETO PARA UMA VIDA MAIS LEVE</h4>
+            <h2 className="text-[#FFF] text-[53px] font-poppins font-bold w-[481px] leading-[53px] mt-[45px]">DESCUBRA NESSE E-BOOK  O SEGREDO PARA EMAGRECER COM SAÚDE.</h2>
+            <h6 className="text-[#FFF] text-base text-center font-poppins mt-[150px]">Compre Agora Mesmo o Seu E-book.</h6>
+            <button className="bg-[#4BC228] text-white text-[25px] font-bold rounded-[40px] w-[337px] h-[65px] mt-[30px] ml-[76px]">
+              <a href="https://pay.kiwify.com.br/qao3xsN">COMPRAR AGORA</a>
+            </button>
+          </div>
+          <div className="ml-[45px]">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              alt="Icone"
+              src="/ebook.png"
+              width={400}
+              height={458}
+              className="object-contain"
             />
-          </a>
+          </div>
         </div>
-      </div>
+        <footer className="mt-[100px] w-full">
+          <Image
+            alt="Icone"
+            src="/footer.png"
+            width={1920}
+            height={100}
+            className="object-contain w-full"
+          />
+        </footer>
+      </main>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="flex pt-[120px] min-h-screen flex-col items-center justify-between bg-[#2A2A2A]">
+        <div className="flex flex-col max-w-[655px]">
+          <h1 className="font-semibold text-[35px] leading-[35px] font-poppins text-white">POR QUE <br />
+            <span className="text-[#7ED957]">COMPRAR NOSSO E-BOOK?</span>
+          </h1>
+          <h2 className="text-[20px] leading-[36px] font-poppins text-white mt-[32px]">Se você está procurando uma maneira de perder peso de forma
+            saudável e duradoura, baixe agora mesmo nosso Guia Definitivo
+            Do Emagrecimento Saudável. Aqui estão alguns motivos pelos
+            quais você deveria considerar adquirir nosso e-book sobre
+            emagrecimento saudável:</h2>
+        </div>
+        <div className="flex items-center gap-[38px] justify-center mt-[60px]">
+          <div>
+            <div className="w-[295px] gap-2.5 flex flex-col justify-start">
+              <h3 className="font-poppins text-xl text-[#4bc228] font-semibold leading-[30px]">Dicas Práticas:</h3>
+              <p className="font-poppins text-[15px] text-white leading-[27px]">
+                Nosso e-book sobre emagrecimento
+                saudável geralmente contêm dicas
+                práticas e fáceis de seguir para ajudar
+                você a mudar seus hábitos
+                alimentares e estilo de vida, de forma
+                que possa manter o peso ideal a longo
+                prazo.
+              </p>
+            </div>
+            <hr className="my-[25px]" />
+            <div className="w-[295px] gap-2.5 flex flex-col justify-start">
+              <h3 className="font-poppins text-xl text-[#4bc228] font-semibold leading-[30px]">Acesso Instantâneo:</h3>
+              <p className="font-poppins text-[15px] text-white leading-[27px]">
+                Comprar nosso ebook para emagrecer
+                saudável é rápido e fácil, e você pode
+                ter acesso imediato ao conteúdo
+                assim que finalizar o download.
+              </p>
+            </div>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              alt="Icone"
+              src="/ebook.png"
+              width={400}
+              height={400}
+              className="object-contain"
+            />
+            <button className="bg-[#4BC228] text-white text-[25px] font-bold rounded-[40px] w-[337px] h-[65px] mt-[30px]">
+              <a href="https://pay.kiwify.com.br/qao3xsN">COMPRAR AGORA</a>
+            </button>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div>
+            <div className="w-[295px] gap-2.5 flex flex-col justify-start">
+              <h3 className="font-poppins text-xl text-[#4bc228] font-semibold leading-[30px]">Informações Especializadas:</h3>
+              <p className="font-poppins text-[15px] text-white leading-[27px]">
+                Nosso e-book de emagrecimento
+                saudável contém informações
+                confiáveis e especializadas sobre como
+                perder peso com saúde, sem precisar
+                seguir dietas malucas ou perigosas.
+              </p>
+            </div>
+            <hr className="my-[25px]" />
+            <div className="w-[295px] gap-2.5 flex flex-col justify-start">
+              <h3 className="font-poppins text-xl text-[#4bc228] font-semibold leading-[30px]">Flexibilidade:</h3>
+              <p className="font-poppins text-[15px] text-white leading-[27px]">
+                Com nosso ebook sobre
+                emagrecimento saudável, você pode
+                aprender a controlar o seu peso no seu
+                próprio ritmo e de acordo com o seu
+                estilo de vida, sem precisar seguir
+                regras rígidas ou horários específicos.
+              </p>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+        <footer className="mt-[100px] w-full">
+          <Image
+            alt="Icone"
+            src="/footer.png"
+            width={1920}
+            height={100}
+            className="object-contain w-full"
+          />
+        </footer>
+      </main>
+    </>
   )
 }
